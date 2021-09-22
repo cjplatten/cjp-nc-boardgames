67cd31df08e87a6d948866420f631e841cc452e1
Carrie <cjplatten@gmail.com>
2021-09-22 08:52:22 +0100

    refeactor GET /api/categories test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,29 +10,16 @@ afterAll(() => db.end());
 describe("/api", () => {
   describe("/categories", () => {
     describe("GET", () => {
-      test("200: responds with an array of category objects,with the properties: slug,  description", () => {
-        return request(app)
-          .get("/api/categories")
-          .expect(200)
-          .then((res) => {
-            res.body.categories.forEach((category) => {
-              expect(category).toMatchObject({
-                slug: expect.any(String),
-                description: expect.any(String),
-              });
-            });
+      test("200: responds with an array of category objects,with the properties: slug,  description", async () => {
+        const res = await request(app)
+        .get("/api/categories")
+        .expect(200);
+        res.body.categories.forEach((category) => {
+          expect(category).toMatchObject({
+            slug: expect.any(String),
+            description: expect.any(String),
           });
-        //   test("200: responds with an array of category objects,with the properties: slug,  description", async () => {
-        //     const res = await request(app)
-        //     // return request(app)
-        //       .get("api/categories")
-        //       .expect(200)
-        //     //   .then((res) => {
-        //         expect(res.body).toMatchObject({
-        //           slug: expect.any(String),
-        //           description: expect.any(String),
-        //         // });
-        //       });
+        });
       });
     });
   });
