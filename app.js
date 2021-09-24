aef64ab614cabb2cdb2592b23e2fec311790a8e9
Carrie <cjplatten@gmail.com>
2021-09-24 15:48:55 +0100

    tidy app.js

--- app.js
+++ app.js
@@ -9,20 +9,6 @@ const {
 const app = express();
 app.use(express.json());
 
-/*
-Essential endpoints
-
-GET /api/categories
-GET /api/reviews/:review_id
-PATCH /api/reviews/:review_id
-GET /api/reviews
-GET /api/reviews/:review_id/comments
-POST /api/reviews/:review_id/comments
-GET /api
-
-All of your endpoints should send the responses specified below in an object, with a key name of what it is that being sent.
-*/
-
 app.use("/api", apiRouter);
 
 app.all("*", (req, res) => {
